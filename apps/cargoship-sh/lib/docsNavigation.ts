e55a295a0b4f50a94462733b2503d81779fb7dae
Matthias Nannt <mail@matthiasnannt.com>
2023-02-17 14:20:47 +0100

    add more docs

--- apps/cargoship-sh/lib/docsNavigation.ts
+++ apps/cargoship-sh/lib/docsNavigation.ts
@@ -1,11 +1,15 @@
 const navigation = [
   {
     title: "Introduction",
-    links: [{ title: "Why Cargoship?", href: "/docs/introduction/why-cargoship" }],
+    links: [
+      { title: "Why Cargoship?", href: "/docs/introduction/why-cargoship" },
+      { title: "Your Benefits", href: "/docs/introduction/your-benefits" },
+    ],
   },
   {
     title: "Basic Usage",
     links: [
+      { title: "Basics", href: "/docs/basics/basics" },
       { title: "Quick Start", href: "/docs/basics/quickstart" },
       { title: "Deployment", href: "/docs/basics/deployment" },
     ],
