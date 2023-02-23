8db8aa5db9232911ffa5f66a702833c4beba13e4
Christopher <christopher@stackocean.io>
2023-02-23 14:38:33 +0100

    filled docks

--- apps/cargoship-sh/components/home/ModelCollection.tsx
+++ apps/cargoship-sh/components/home/ModelCollection.tsx
@@ -85,7 +85,7 @@ export default function ModelCollection() {
             <Link
               href="/collections"
               className="rounded-md bg-sky-500 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm transition-all ease-in-out hover:scale-105 hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400">
-              Explore Models
+              Check out the models
             </Link>
         </div>
       </div>
