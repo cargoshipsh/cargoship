a767e3dc6bf6e9a4ad6f76e6712c3d86e5884198
Matthias Nannt <mail@matthiasnannt.com>
2023-03-02 14:19:21 +0100

    increade margin on homepage between elements

--- apps/cargoship-sh/components/home/ModelCollection.tsx
+++ apps/cargoship-sh/components/home/ModelCollection.tsx
@@ -58,7 +58,7 @@ import BackgroundGradient from "./BackgroundGradient";
 
 export default function ModelCollection() {
   return (
-    <div className="relative py-10">
+    <div className="relative py-20">
       <div className="absolute inset-x-0 top-[calc(100%-24rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-48rem)]">
         <BackgroundGradient rotate={true} />
       </div>
