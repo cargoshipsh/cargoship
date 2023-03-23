f3941833ad28cfb993ab58c8e33029dc296269da
Matthias Nannt <mail@matthiasnannt.com>
2023-03-23 10:39:06 +0100

    layout fixes

--- apps/cargoship-sh/components/shared/GithubInfo.tsx
+++ apps/cargoship-sh/components/shared/GithubInfo.tsx
@@ -4,7 +4,7 @@ export default function GithubInfo() {
   return (
     <Link
       href="https://github.com/cargoshipsh/cargoship"
-      className="flex h-12 cursor-pointer items-center justify-center bg-sky-100 text-sm font-medium text-slate-900">
+      className="flex cursor-pointer items-center justify-center bg-sky-100 px-5 py-3 text-center text-sm font-medium text-slate-900">
       ⭐️ Cargoship is open source on Github. Help us by giving us a star! ⭐️
     </Link>
   );
